9f1981ef8747936e084168aea68c0270d247543d
jdivyadharshini2005-oss <jdivyadharshini2005@gmail.com>
2026-02-09 11:19:21 +0530

    Add files via upload

---
+++ Array/array2D/2Darray-15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int arr[r][c];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int count = 0;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            for (int k = 0; k < j; k++) {
+                if (arr[i][j]%arr[k][j]==0) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
