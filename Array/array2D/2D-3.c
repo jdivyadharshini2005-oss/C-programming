6df6ae29b3d10a4550512c91ea502f4653a5f9ec
jdivyadharshini2005-oss <jdivyadharshini2005@gmail.com>
2026-02-09 09:55:03 +0530

    Add files via upload

---
+++ Array/array2D/2D-3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main() {
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++) {
+        for(int j=0;j<c;j++) {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int unique=0;
+    for (int i=0;i<r;i++) {
+        int count=0;
+        for (int j=0;j<c;j++) {
+            for (int k=0;k<r;k++) {
+                if (arr[i][j]==arr[i][k]) {
+                    count++;
+                }
+            }
+        }
+        if (count==3) {
+            unique++;
+            printf("%d",unique);
+        }
+    }
+    return 0;
+}
+
